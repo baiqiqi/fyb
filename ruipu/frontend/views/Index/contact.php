8499d00e632260a87fce63f4b64bc6c566c71371
qq <1060282752@qq.com>
2016-04-15 07:44:44 +0800

    白琪琪 4.15早上7.45提交

--- ruipu/frontend/views/Index/contact.php
+++ ruipu/frontend/views/Index/contact.php
@@ -4,21 +4,66 @@
 		<div class="container">
 			<div class="row">
 				<div class="col-md-7 map">
-				  <div class="map">
-					<iframe width="100%" height="350" frameborder="0" scrolling="no" marginheight="0" marginwidth="0" src="https://maps.google.co.in/maps?f=q&amp;source=s_q&amp;hl=en&amp;geocode=&amp;q=Lighthouse+Point,+FL,+United+States&amp;aq=4&amp;oq=light&amp;sll=26.275636,-80.087265&amp;sspn=0.04941,0.104628&amp;ie=UTF8&amp;hq=&amp;hnear=Lighthouse+Point,+Broward,+Florida,+United+States&amp;t=m&amp;z=14&amp;ll=26.275636,-80.087265&amp;output=embed"></iframe><br><small><a href="https://maps.google.co.in/maps?f=q&amp;source=embed&amp;hl=en&amp;geocode=&amp;q=Lighthouse+Point,+FL,+United+States&amp;aq=4&amp;oq=light&amp;sll=26.275636,-80.087265&amp;sspn=0.04941,0.104628&amp;ie=UTF8&amp;hq=&amp;hnear=Lighthouse+Point,+Broward,+Florida,+United+States&amp;t=m&amp;z=14&amp;ll=26.275636,-80.087265" style="color:#666;text-align:left;font-size:12px"></a></small>
-				  </div>
+				  
+<div class="shop_top">
+		<div class="container">
+			<div class="row shop_box-top">
+				<div class="col-md-3 shop-1">
+					<h3>牙医</h3>
+					<img src="images/6.jpg" class="img-responsive" alt="">	
+					<ul>
+						<li><a href="#"><span></span>Adhesives</a></li>
+						<li><a href="#"><span></span>Cements</a></li>
+						<li><a href="#"><span></span>Composites</a></li>
+						<li><a href="#"><span></span>Curing Lights</a></li>
+						<li><a href="#"><span></span>Finishing & Polishing</a></li>
+						<li><a href="#"><span></span>Glass Ionomers</a></li>
+					</ul>
 				</div>
-				<div class="col-md-5 tes">
-					<p class="m_8">Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat. Ut wisi enim ad minim veniam, quis nostrud exerci tation ullamcorper suscipit lobortis nisl ut aliquip ex ea commodo consequat. Duis autem vel eum iriure dolor in hendrerit in vulputate velit esse molestie consequat</p>
-					<div class="address">
-				                <p>500 Lorem Ipsum Dolor Sit,</p>
-						   		<p>22-56-2-9 Sit Amet, Lorem,</p>
-						   		<p>USA</p>
-				   		<p>Phone : (00) 222 666 444</p>
-				   		<p>Fax : (000) 000 00 00 0</p>
-				 	 	<p>Email : <a href="mailto:info@example.com">info@sitename.com </a></p>
-				   </div>
+				<div class="col-md-3 shop-1">
+					<h3>内科</h3>
+					<img src="images/3.jpg" class="img-responsive" alt="">	
+					<ul>
+						<li><a href="#"><span></span>Littmann</a></li>
+						<li><a href="#"><span></span>Iv sitecare </a></li>
+						<li><a href="#"><span></span>Compression Bandage</a></li>
+						<li><a href="#"><span></span>Wound post of care</a></li>
+						<li><a href="#"><span></span>Handhygeine</a></li>
+						<li><a href="#"><span></span>Peri Operative</a></li>
+					</ul>
 				</div>
+				<div class="col-md-3 shop-1">
+					<h3>外科</h3>
+					<img src="images/4.jpg" class="img-responsive" alt="">	
+					<ul>
+						<li><a href="#"><span></span>Preventive</a></li>
+						<li><a href="#"><span></span>Temporization</a></li>
+						<li><a href="#"><span></span>Tinear Base Kits</a></li>
+						<li><a href="#"><span></span>Curing Lights</a></li>
+						<li><a href="#"><span></span>Finishing & Polishing</a></li>
+						<li><a href="#"><span></span>Glass Ionomers</a></li>
+					</ul>
+				</div>
+				
+				<div class="col-md-3 shop-1">
+					<h3>神经科</h3>
+					<img src="images/2.jpg" class="img-responsive" alt="">	
+					<ul>
+						<li><a href="#"><span></span>Skin Integrity</a></li>
+						<li><a href="#"><span></span>Immbolisation</a></li>
+						<li><a href="#"><span></span>Patient Monitoring</a></li>
+						<li><a href="#"><span></span>Cold & Hot Pack</a></li>
+						<li><a href="#"><span></span>Eye Pack</a></li>
+						<li><a href="#"><span></span>Temporization</a></li>
+					</ul>
+				</div>
+			</div>
+			
+			</div>
+	   </div>
+
+				</div>
+
 			</div>
 			<div class="row">
 				<div class="col-md-12 contact">
